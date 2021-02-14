7e4a02a0c1bb6684fe82dfc349e811d1896fcf0c
Matt Pollack <matt.pck@gmail.com>
2021-02-14 00:28:52 -0500

    StandardGame and MakesALegalMove

--- engine.py
+++ engine.py
@@ -27,6 +27,15 @@ game notation
 
 """
 
+import random
+
+class IllegalMove(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class Concede(Exception):
+    pass
+
 class Position(object):
     def __init__(self, x, y):
         self.x = x
@@ -52,9 +61,11 @@ class Piece(object):
         self.prefix = prefix
         self.color = color
 
-    # NOTE: maybe consider some sort of exception handling here, not really sure what's idiomatic python
     def canMove(self, start, end, board):
-        return False
+        raise Exception("Attempt to canMove Piece base class (not implemented)")
+    
+    def copy(self):
+        raise Exception("Attempt to copy Piece base class (not implemented)")
 
 class Pawn(Piece):
     def __init__(self, color):
@@ -92,6 +103,9 @@ class Pawn(Piece):
             return otherPiece == None
         
         return otherPiece != None and otherPiece.color != self.color
+    
+    def copy(self):
+        return Pawn(self.color)
 
 class Knight(Piece):
     def __init__(self, color):
@@ -123,6 +137,9 @@ class Knight(Piece):
         otherPiece = board.getPiece(end)
 
         return otherPiece == None or (otherPiece != None and otherPiece.color != self.color)
+    
+    def copy(self):
+        return Knight(self.color)
 
 class Bishop(Piece):
     def __init__(self, color):
@@ -136,12 +153,12 @@ class Bishop(Piece):
         
         relative = end.sub(start)
 
-        if abs(relative.y/relative.x) != 1:
+        if relative.y == 0 or relative.x == 0 or abs(relative.y/relative.x) != 1:
             return False
 
         xSign = relative.x/abs(relative.x)
         ySign = relative.y/abs(relative.y)
-        distance = abs(relative.x)-1
+        distance = abs(relative.x)
 
         for i in range(1, distance):
             x = i*xSign
@@ -153,6 +170,9 @@ class Bishop(Piece):
         otherPiece = board.getPiece(end)
 
         return otherPiece == None or (otherPiece != None and otherPiece.color != self.color)
+
+    def copy(self):
+        return Bishop(self.color)
 
 class Rook(Piece):
     def __init__(self, color):
@@ -176,7 +196,7 @@ class Rook(Piece):
         if abs(relative.y) > 0:
             ySign = relative.y/abs(relative.y)
         
-        distance = abs(relative.x) + abs(relative.y) - 1
+        distance = abs(relative.x) + abs(relative.y)
 
         for i in range(1, distance):
             x = i*xSign
@@ -189,12 +209,18 @@ class Rook(Piece):
 
         return otherPiece == None or (otherPiece != None and otherPiece.color != self.color)
 
+    def copy(self):
+        return Rook(self.color)
+
 class Queen(Piece):
     def __init__(self, color):
         super().__init__("q", color)
 
     def canMove(self, start, end, board):
         return Rook(self.color).canMove(start, end, board) or Bishop(self.color).canMove(start, end, board)
+
+    def copy(self):
+        return Queen(self.color)
 
 class King(Piece):
     def __init__(self, color):
@@ -225,28 +251,42 @@ class King(Piece):
         
         return False
 
+    def copy(self):
+        return King(self.color)
+
 class Board:
     def __init__(self):
         self.__grid = {}
 
     def __str__(self):
-        res = ""
+        res = "  0-1-2-3-4-5-6-7-\n"
 
         for y in range(7, -1, -1):
+            res += f'{y} '
+
             for x in range(0, 8, 1):
                 piece = self.getPiece(Position(x, y))
-                
+
                 if piece == None:
                     res += "  "
                 else:
-                    res += f'{piece.color}{piece.prefix}'
+                    color = " "
+                    prefix = piece.prefix
+
+                    if piece.color == "b":
+                        color = "_"
+                    
+                    if piece.prefix == " ":
+                        prefix = "p"
+                
+                    res += f'{color}{prefix}'
 
                 if y < 7:
                     res += ""
 
-            res += "\n"
+            res += f' {y}\n'
 
-        return res + "<><><><><><><><>"
+        return res + "  0-1-2-3-4-5-6-7-"
     
     def getPiece(self, position):
         try:
@@ -283,18 +323,31 @@ class Board:
 
     def movePiece(self, start, end):
         if not self.inBounds(start) or not self.inBounds(end) or start == end:
-            return None
+            raise IllegalMove((start, end))
 
         piece = self.getPiece(start)
 
         if piece == None:
-            return None
+            raise IllegalMove((start, end))
 
         if not piece.canMove(start, end, self):
-            return None
+            raise IllegalMove((start, end))
 
         self.removePiece(start)
-        return self.placePiece(piece, end)
+        otherPiece = self.placePiece(piece, end)
+
+        # Revert the move it would cause a check, could be more efficient of a check?
+        if piece.color in self.playersInCheck():
+            self.placePiece(piece, start)
+
+            if otherPiece != None:
+                self.placePiece(otherPiece, end)
+            else:
+                self.removePiece(end)
+
+            raise IllegalMove((start, end))
+
+        return otherPiece
     
     def playersInCheck(self):
         colors = {}
@@ -307,52 +360,161 @@ class Board:
                     colors[piece.color] = True
         
         return colors.keys()
+    
+    # NOTE: brute force option, could be a better method for this
+    def playerInCheckmate(self, color):
+        for pos in self.__grid.keys():
+            piece = self.__grid[pos]
+
+            if piece.color != color:
+                continue
+
+            for x in range(8):
+                for y in range(8):
+                    if piece.canMove(pos, Position(x, y), self):
+                        return False
+
+        return True
+    
+    def copy(self):
+        new = Board()
+
+        for pos in self.__grid:
+            new.placePiece(self.__grid[pos].copy(), pos)
         
+        return new
 
+class StandardGame(object):
+    def __init__(self, playerA, playerB):
+        self.board = Board()
+        self.moveNum = 0
+        self.winner = None
 
+        # Place all pieces for each team
+        for x in range(8):
+           self.board.placePiece(Pawn("w"), Position(x, 1))
+           self.board.placePiece(Pawn("b"), Position(x, 6))
+        
+        for pair in [("w", 0), ("b", 7)]:
+            color = pair[0]
+            y = pair[1]
 
-# TEST CONFIGURATION
-board = Board()
-board.placePiece(Pawn("b"), Position(0, 2))
-board.placePiece(Pawn("w"), Position(1, 1))
-board.placePiece(Knight("b"), Position(1, 4))
-board.placePiece(Bishop("w"), Position(3, 5))
-board.placePiece(Rook("b"), Position(0, 5))
-board.placePiece(Queen("b"), Position(5, 2))
-board.placePiece(Queen("w"), Position(2, 0))
-board.placePiece(King("w"), Position(6, 6))
+            self.board.placePiece(Rook(color), Position(0, y))
+            self.board.placePiece(Rook(color), Position(7, y))
+            self.board.placePiece(Knight(color), Position(1, y))
+            self.board.placePiece(Knight(color), Position(6, y))
+            self.board.placePiece(Bishop(color), Position(2, y))
+            self.board.placePiece(Bishop(color), Position(5, y))
+            self.board.placePiece(Queen(color), Position(3, y))
+            self.board.placePiece(King(color), Position(4, y))
+        
+        # Choose the first player
+        coin = random.randrange(0, 2)
 
-print(board)
-print(board.playersInCheck())
+        if coin == 1:
+            self.white = playerA
+            self.black = playerB
+        else:
+            self.white = playerB
+            self.black = playerA
 
-board.movePiece(Position(1, 1), Position(0, 2))
-print(board)
-print(board.playersInCheck())
+    def turn(self):
+        if not self.winner == None:
+            return self.winner
 
-board.movePiece(Position(1, 4), Position(0, 2))
-print(board)
-print(board.playersInCheck())
+        player = None
+        color = None
 
-board.movePiece(Position(3, 5), Position(0, 2))
-print(board)
-print(board.playersInCheck())
+        if self.moveNum % 2 == 0:
+            player = self.white
+            color = "w"
+        else:
+            player = self.black
+            color = "b"
 
-board.movePiece(Position(0, 5), Position(0, 2))
-print(board)
-print(board.playersInCheck())
+        self.moveNum += 1
 
-board.movePiece(Position(2, 0), Position(0, 2))
-print(board)
-print(board.playersInCheck())
+        move = None
 
-board.movePiece(Position(5, 2), Position(0, 2))
-print(board)
-print(board.playersInCheck())
+        # Players which don't catch errors lose
+        try:
+            move = player.Do(self.board.copy(), color)
+            start = move[0]
+            end = move[1]
+            self.board.movePiece(start, end)
+        except Concede:
+            if color == "w":
+                self.winner = "b"
+            else:
+                self.winner = "w"
 
-board.movePiece(Position(0, 2), Position(0, 6))
-print(board)
-print(board.playersInCheck())
+            return None
 
-board.placePiece(Pawn("w"), Position(3, 6))
-print(board)
-print(board.playersInCheck())
+        # Check for checkmate
+        if self.board.playerInCheckmate(color):
+            if color == "w":
+                self.winner = "b"
+            else:
+                self.winner = "w"
+
+        # Check that there aren't just two kings
+        pieces = self.board.getPieces()
+        justKings = True
+
+        for p in pieces:
+            if not isinstance(p[0], King):
+                justKings = False
+                break
+        
+        if justKings:
+            self.winner = "draw"
+        
+        # TODO: check for draws where the other player has no more legal moves
+        
+        return None
+
+class MakesALegalMove(object):
+    def __init__(self):
+        super().__init__()
+
+    def Do(self, board, color):
+        pieces = board.getPieces()
+        moves = []
+
+        for pair in pieces:
+            piece = pair[0]
+            start = pair[1]
+
+            if piece.color != color:
+                continue
+
+            for x in range(8):
+                for y in range(8):
+                    end = Position(x, y)
+
+                    if start != end and piece.canMove(start, end, board):
+                        moves.append((start, end))
+
+        indices = list(range(0, len(moves)))
+        random.shuffle(indices)
+
+        for i in indices:
+            move = moves[i]
+
+            try:
+                board.movePiece(move[0], move[1])
+            except IllegalMove:
+                continue
+
+            return move
+        
+        raise Concede
+
+game = StandardGame(MakesALegalMove(), MakesALegalMove())
+print (game.board)
+
+while game.winner == None:
+    game.turn()
+    print (game.board)
+
+print(f'The winner is {game.winner}!')
